2a2b274467a5a22d758f06a3a21e3b3d076ebfdc
Olivier Ertz <Olivier.Ertz@heig-vd.ch>
2015-10-06 18:06:49 +0200

    Ex1A OK.

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@
  * Definition of global JS application parameters
  **************************************************/
 
- /*
+/*
  * With the Boundless GeoServer you can play with layers: 
  * - ne_10m_admin_0_countries
  * - ne_10m_lakes
  * - etc (see http://demo.boundlessgeo.com/geoserver/wms?REQUEST=GetCapabilities)
  */
- var blWMS = "http://demo.boundlessgeo.com/geoserver/wms";
+var blWMS = "http://demo.boundlessgeo.com/geoserver/wms";
 
 /*
  * With the MapCentia MapServer you can play with layers: 
@@ -17,8 +17,8 @@
  * - public.cantonsch_region
  * - etc (see https://eu1.mapcentia.com/wms/oertz/public?service=WMS&request=GetCapabilities)
  */
- var mcWMS = "https://eu1.mapcentia.com/wms/oertz/public";
- var mcWFS = "https://eu1.mapcentia.com/wfs/oertz/public/4326";
+var mcWMS = "https://eu1.mapcentia.com/wms/oertz/public";
+var mcWFS = "https://eu1.mapcentia.com/wfs/oertz/public/4326";
 
 /*
  * With our rhcloud GeoServer you can play with layers: 
@@ -26,15 +26,15 @@
  * - cities
  * - etc (see http://geoserver-heig.rhcloud.com/geoinf/ows?service=WMS&version=1.1.0&request=GetCapabilities)
  */
- var myOWS = "http://geoserver-heig.rhcloud.com/geoinf/ows";
- 
- /*
+var myOWS = "http://geoserver-heig.rhcloud.com/geoinf/ows";
+
+/*
  * With the Swisstopo MapServer you can play with layers: 
  * - ch.swisstopo.swissboundaries3d-kanton-flaeche.fill
  * - ch.swisstopo.swissboundaries3d-gemeinde-flaeche.fill
  * - etc (see http://wms.geo.admin.ch/?REQUEST=GetCapabilities&SERVICE=WMS&VERSION=1.0.0&lang=fr)
  */
- var chWMS = "https://wms.geo.admin.ch";
+var chWMS = "https://wms.geo.admin.ch";
 
 /* 
  * Insertion of <script> and <stylesheet> elements to load required libraries :
@@ -51,3 +51,13 @@ document.write(scriptTag);
 
 scriptTag = '<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>';
 document.write(scriptTag);
+
+/*
+ * May be some useful functions
+ */
+function showExtent(bbox) {
+    vectors = new ol.layer.Vector({source: new ol.source.Vector()});
+    map.addLayer(vectors);
+    vectors.getSource().addFeatures([new ol.Feature({geometry: new ol.geom.Polygon.fromExtent(bbox)})]);
+
+}
